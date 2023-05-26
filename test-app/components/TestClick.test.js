5d99a91c1973e71fa5000ea153684bcf6d9841ed
HYK424 <turtle424@naver.com>
2023-05-26 14:09:45 +0900

    test: CI

--- test-app/components/TestClick.test.js
+++ test-app/components/TestClick.test.js
@@ -1,9 +1,9 @@
-import TestClick from "./TestClick";
+import TestClick from "@/components/TestClick";
 import { render, screen } from "@testing-library/react";
 
 describe("<TestClick />", () => {
   it("render num", () => {
-    render(<TestClick />);
+    const { container } = render(<TestClick />);
     const num = screen.getByText("num");
     expect(num).toBeInTheDocument();
   });
