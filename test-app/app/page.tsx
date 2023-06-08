9f20db436eb8b77f89e38c9de6f2327805c5e05c
HYK424 <turtle424@naver.com>
2023-06-08 21:27:22 +0900

    style: 주석 추가 및 로그 출력 삭제

--- test-app/app/page.tsx
+++ test-app/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
     const utc = curr.getTime() + curr.getTimezoneOffset() * 60 * 1000;
     const KR_TIME_DIFF = 9 * 60 * 60 * 1000; //한국 시간(KST)은 UTC시간보다 9시간 더 빠르므로 9시간을 밀리초 단위로 변환.
     const kr_curr = new Date(utc + KR_TIME_DIFF); //UTC 시간을 한국 시간으로 변환하기 위해 utc 밀리초 값에 9시간을 더함.
-    console.log(`${process.env.NEXT_PUBLIC_API_KEY}`);
+    console.log(`메인 페이지 완성까지 본 페이지가 유지됩니다.`);
     return kr_curr;
   };
 
